0caeac7a5d470b29d52be2a1627f4acaa813151f
Harry Antill <hja38@cam.ac.uk>
2022-02-07 12:52:02 +0000

    1D function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,12 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def rivers_with_station(stations):
+    """Takes a list of stations and returns a set of all the rivers
+    in alphabetic order upon which those stations are located"""
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
+    rivers = sorted(rivers)
+    return rivers

1059b55ba944f2159499063433872b4f155c852e
Harry Antill <hja38@cam.ac.uk>
2022-02-09 14:49:31 +0000

    Added functions for 1D and 1E to geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -50,6 +50,57 @@ def stations_within_radius(stations, centre, r):
             stations_within = stations_within
     return stations_within
 
-    
+
+#Task 1D
+
+def rivers_with_station(stations):
+    """Takes a list of stations and returns a set of all the rivers
+    in alphabetic order upon which those stations are located"""
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
+    rivers = sorted(rivers)
+    return rivers
+
+def stations_by_river(stations, river):
+    """Takes a list of stations and returns a list of all the station names
+    on a specific river in alphabetic order"""
+    station_names = []
+    for station in stations:
+        if station.river == river:
+            station_names.append(station.name)
+    station_names = sorted(station_names)
+    return station_names
+
+def rivers_by_station_number(stations, N):
+    """Take a list of stations and return the N rivers with the most stations upon
+    them, in the form of a list of tuples in descending order"""
+
+    # use another function in geo to build list of non-repeated rivers
+    rivers = rivers_with_station(stations)
+
+    # initialise list
+    rivers_with_count = []
+
+    for river in rivers:
+        count = 0
+        for station in stations:
+            if station.river == river:
+                count += 1
+
+        # now that the number of times the river appears in the station list
+        # has been determined, the river can be appended
+        rivers_with_count.append((river, count))
+
+    # sort list according to number of rivers
+    rivers_with_count = sorted(
+        rivers_with_count, key=lambda x: x[1], reverse=True)
+
+    # if the next river has the same number of stations, N must be increased
+    # to include this river
+    while rivers_with_count[N - 1][1] == rivers_with_count[N][1]:
+        N += 1
+
+    return rivers_with_count[:N]
 
 

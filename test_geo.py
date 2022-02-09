228e33109c66b303791a97fc396bd3d3b0bb8682
Harry Antill <hja38@cam.ac.uk>
2022-02-09 15:09:27 +0000

    test functions added for D and E

--- test_geo.py
+++ test_geo.py
@@ -1,8 +1,7 @@
 from floodsystem.utils import sorted_by_key  # noqa
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import MonitoringStation
-from floodsystem.geo import stations_by_distance
-from floodsystem.geo import stations_within_radius
+from floodsystem.geo import stations_within_radius, stations_by_distance, rivers_with_station, stations_by_river, rivers_by_station_number
 from floodsystem.geo import haversine_formula
 from floodsystem.station import inconsistent_typical_range_stations
 
@@ -25,3 +24,25 @@ def test_stations_within_radius(stations):
         assert haversine_formula(station.coord[0], station.coord[1], centre[0], centre[1]) <= r
 
 
+def test_rivers_with_station():
+    """Tests that the function has at least 800 rivers inc. Thames"""
+    stations = build_station_list()
+    rivers = rivers_with_station(stations)
+    assert len(rivers) >= 800
+    assert "Thames" in rivers
+
+def test_stations_by_river():
+    """Check that certain stations are produced in the list for certain rivers"""
+    stations = build_station_list()
+    assert "Armley" in stations_by_river(stations, "River Aire")
+    assert "Benson Lock" in stations_by_river(stations, "Thames")
+
+
+def test_rivers_by_station_number():
+    """Tests the length of a few outputs and checks order of rivers"""
+    stations = build_station_list()
+    assert len(rivers_by_station_number(stations, 1)) >= 1
+    assert len(rivers_by_station_number(stations, 10)) >= 10
+    assert len(rivers_by_station_number(stations, 30)) >= 30
+    biggest_river = rivers_by_station_number(stations, 1)
+    assert biggest_river[0][0] == "Thames"
